24386fa8a5be45dbe0acf2fb5fbd693e4dafa099
Amir Nurhakim <amir.zaid@silentmode.my>
2025-10-28 20:57:43 +0800

    fix(route): refresh to the correct page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '^(?!api|build|storage|favicon\\.ico|robots\\.txt).*');
